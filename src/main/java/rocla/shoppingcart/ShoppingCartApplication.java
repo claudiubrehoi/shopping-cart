70ab4410edec068cc06e06d74d217237d07b5840
claudiu.brehoi <Djtommy#21>
2019-10-10 23:51:38 +0300

    fix deployment

--- src/main/java/rocla/shoppingcart/ShoppingCartApplication.java
+++ src/main/java/rocla/shoppingcart/ShoppingCartApplication.java
@@ -3,11 +3,18 @@ package rocla.shoppingcart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class ShoppingCartApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingCartApplication.class, args);
+	}
+
+	@PostConstruct
+	public void test() {
+		System.out.println("WTF YO");
 	}
 
 }
